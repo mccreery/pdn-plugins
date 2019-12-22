045df80ab36b104565bfb9c7b46afb3e89e502b5
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2019-12-22 22:42:46 +0000

    Add assembly info to template project

---
+++ Shared/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+/*
+ * This information is shared across all projects, so it is included in the
+ * template project and (hopefully) all other projects.
+ */
+[assembly: AssemblyProduct("Assorted Plugins")]
+[assembly: AssemblyCompany("Sam McCreery")]
+[assembly: AssemblyCopyright("© 2019 Sam McCreery")]
+
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+
+[assembly: ComVisible(false)]
--- Template/Properties/AssemblyInfo.cs
+++ Template/Properties/AssemblyInfo.cs
@@ -1,17 +1,7 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
+// See ..\Shared\AssemblyInfo.cs for other attributes
 [assembly: AssemblyTitle("Template plugin for paint.net")]
 [assembly: AssemblyDescription("")]
-#if DEBUG
-[assembly: AssemblyConfiguration("Debug")]
-#else
-[assembly: AssemblyConfiguration("Release")]
-#endif
-[assembly: AssemblyCompany("Sam McCreery")]
-[assembly: AssemblyProduct("Template")]
-[assembly: AssemblyCopyright("© 2019 Sam McCreery")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: ComVisible(false)]
 [assembly: AssemblyVersion("0.1.*")]
