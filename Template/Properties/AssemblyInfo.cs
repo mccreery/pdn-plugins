be2fefb529f9570616a539d1663c865913bcbad3
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2019-12-22 18:47:54 +0000

    Add simple template for working outside VS

---
+++ Template/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Template plugin for paint.net")]
+[assembly: AssemblyDescription("")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Sam McCreery")]
+[assembly: AssemblyProduct("Template")]
+[assembly: AssemblyCopyright("© 2019 Sam McCreery")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.*")]
