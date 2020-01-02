3733bc6475890e5cc440553d3491bd9cc026b380
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-01-02 22:07:23 +0000

    Start work on installer (may switch UI)

---
+++ Installer/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Reflection.PortableExecutable;
+using System.Runtime.Loader;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Installer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal Install Install => new Install(this);
+
+        public BindingList<PluginEntry> Plugins { get; } = new BindingList<PluginEntry>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadPluginEntries();
+            DataContext = this;
+        }
+
+        void LoadPluginEntries()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            foreach (string resource in assembly.GetManifestResourceNames())
+            {
+                if (resource.EndsWith(".dll"))
+                {
+                    PluginEntry entry = new PluginEntry(resource, "C:\\Program Files\\paint.net\\Effects\\");
+                    Plugins.Add(entry);
+                }
+            }
+        }
+    }
+
+    class Install : ICommand
+    {
+        private readonly MainWindow window;
+
+        internal Install(MainWindow window)
+        {
+            this.window = window;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            //window.pluginsGrid.GetBindingExpression(DataGrid.ItemsSourceProperty).UpdateSource();
+        }
+    }
+}
---
+++ Installer/PluginEntry.cs
@@ -0,0 +1,57 @@
+﻿using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Reflection.Metadata.Ecma335;
+using System.Reflection.PortableExecutable;
+using System.Windows;
+
+namespace Installer
+{
+    public class PluginEntry
+    {
+        private readonly string resourceName;
+
+        public string FileName { get; }
+        public string Description { get; }
+        internal string TargetDir { get; set; }
+
+        internal static readonly DependencyProperty TargetDirProperty = DependencyProperty.Register(
+            nameof(TargetDir), typeof(string), typeof(PluginEntry));
+
+        private string TargetPath => Path.Combine(TargetDir, FileName);
+
+        public bool Installed
+        {
+            get => File.Exists(TargetPath);
+            set
+            {
+                if (value && !Installed)
+                {
+                    using (Stream stream = Assembly.GetEntryAssembly().GetManifestResourceStream(resourceName),
+                        file = new FileStream(TargetPath, FileMode.Create))
+                    {
+                        stream.CopyTo(file);
+                    }
+                }
+                else if (!value && Installed)
+                {
+                    File.Delete(TargetPath);
+                }
+            }
+        }
+
+        public PluginEntry()
+        {
+        }
+
+        internal PluginEntry(string resourceName, string targetDir)
+        {
+            this.resourceName = resourceName;
+
+            FileName = string.Join('.', resourceName.Split('.').TakeLast(2));
+            Description = string.Empty;
+            TargetDir = targetDir;
+        }
+    }
+}
