86d16f694cc1c4bc8f2705cd10110c9eb714a8d4
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2020-02-13 22:48:18 +0000

    Remove old template directory

--- Template/Properties/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Reflection;
-
-// See SharedAssemblyInfo.cs for other attributes
-[assembly: AssemblyTitle("Template")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyVersion("0.1")]
-
-[assembly: AssemblyConfiguration("tag 1|tag 2")]
