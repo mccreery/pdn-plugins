d7abb8726eee16853c2d517b864d568673a7549d
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-01-20 17:40:05 +0000

    Use jagged float array instead of 2d double array

--- DropShadow/Kernel.cs
+++ DropShadow/Kernel.cs
@@ -8,20 +8,22 @@ namespace AssortedPlugins.DropShadow
     {
         private readonly Size size;
         private readonly Size anchor;
-        private readonly double[,] kernelAlpha;
+        private readonly float[][] kernelAlpha;
 
         public Kernel(Bitmap image)
         {
             size = image.Size;
             anchor = new Size(image.Width / 2, image.Height / 2);
 
-            kernelAlpha = new double[image.Height, image.Width];
+            kernelAlpha = new float[image.Height][];
             for(int y = 0; y < image.Height; y++)
             {
+                float[] row = new float[image.Width];
                 for(int x = 0; x < image.Width; x++)
                 {
-                    kernelAlpha[y, x] = image.GetPixel(x, y).A / 255.0;
+                    row[x] = image.GetPixel(x, y).A / 255.0f;
                 }
+                kernelAlpha[y] = row;
             }
         }
 
@@ -34,15 +36,17 @@ namespace AssortedPlugins.DropShadow
             byte maxAlpha = 0;
             for (int y = bounds.Top; y < bounds.Bottom; y++)
             {
+                float[] row = kernelAlpha[y - location.Y];
+
                 for (int x = bounds.Left; x < bounds.Right; x++)
                 {
                     byte alpha = surface[x, y].A;
 
-                    alpha = (byte)Math.Round(alpha * kernelAlpha[y - location.Y, x - location.X]);
+                    alpha = (byte)Math.Round(alpha * row[x - location.X]);
                     maxAlpha = Math.Max(maxAlpha, alpha);
 
                     // Short circuit case - we can't get any more opaque
-                    if(maxAlpha == 255)
+                    if (maxAlpha == 255)
                     {
                         return maxAlpha;
                     }
