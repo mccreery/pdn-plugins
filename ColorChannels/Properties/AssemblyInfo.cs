45d9a3c5d304ec7ff6932b22a73be3bd40a4a992
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-01-21 04:02:33 +0000

    Add Color Channels adjustment effect

---
+++ ColorChannels/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+// See SharedAssemblyInfo.cs for other attributes
+[assembly: AssemblyTitle("Color Channels")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyVersion("0.1")]
+
+[assembly: AssemblyConfiguration("")]
