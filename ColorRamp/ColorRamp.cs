33cc9d529404ae240b23538e562b8ad810f23ecb
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-04-21 01:25:22 +0100

    Add empty Color Ramp plugin

---
+++ ColorRamp/ColorRamp.cs
@@ -0,0 +1,68 @@
+using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection;
+using PaintDotNet;
+using PaintDotNet.Effects;
+using PaintDotNet.IndirectUI;
+using PaintDotNet.PropertySystem;
+
+namespace AssortedPlugins.ColorRamp
+{
+    [PluginSupportInfo(typeof(DefaultPluginInfo))]
+    public class ColorRamp : PropertyBasedEffect
+    {
+        private int exampleProperty;
+
+        public ColorRamp() : base(
+            typeof(ColorRamp).Assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title,
+            new Bitmap(typeof(ColorRamp), "icon.png"),
+            SubmenuNames.Render,
+            new EffectOptions() { Flags = EffectFlags.Configurable })
+        {
+        }
+
+        protected override ControlInfo OnCreateConfigUI(PropertyCollection props)
+        {
+            ControlInfo configUI = CreateDefaultConfigUI(props);
+
+            configUI.SetPropertyControlType(nameof(exampleProperty), PropertyControlType.ColorWheel);
+            configUI.SetPropertyControlValue(nameof(exampleProperty), ControlInfoPropertyNames.DisplayName, "Example Property");
+
+            return configUI;
+        }
+
+        protected override PropertyCollection OnCreatePropertyCollection()
+        {
+            List<Property> props = new List<Property>();
+            props.Add(new Int32Property(nameof(exampleProperty), (int)(uint)EnvironmentParameters.PrimaryColor));
+
+            return new PropertyCollection(props);
+        }
+
+        protected override void OnCustomizeConfigUIWindowProperties(PropertyCollection props)
+        {
+            base.OnCustomizeConfigUIWindowProperties(props);
+            props[ControlInfoPropertyNames.WindowTitle].Value = typeof(ColorRamp).Assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
+        }
+
+        protected override void OnSetRenderInfo(PropertyBasedEffectConfigToken newToken, RenderArgs dstArgs, RenderArgs srcArgs)
+        {
+            base.OnSetRenderInfo(newToken, dstArgs, srcArgs);
+            exampleProperty = newToken.GetProperty<Int32Property>(nameof(exampleProperty)).Value;
+        }
+
+        protected override void OnRender(Rectangle[] renderRects, int startIndex, int length)
+        {
+            int endIndex = startIndex + length;
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                Render(DstArgs.Surface, SrcArgs.Surface, renderRects[i]);
+            }
+        }
+
+        void Render(Surface dst, Surface src, Rectangle rect)
+        {
+        }
+    }
+}
---
+++ ColorRamp/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+// See SharedAssemblyInfo.cs for other attributes
+[assembly: AssemblyTitle("Color Ramp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyVersion("0.1")]
+
+[assembly: AssemblyConfiguration("tag 1|tag 2")]
