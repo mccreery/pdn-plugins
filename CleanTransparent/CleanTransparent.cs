7cdbf447c468d5b3d9534fd0e3b1a1e27044d4b9
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-04-21 15:33:57 +0100

    Add empty project

---
+++ CleanTransparent/CleanTransparent.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Reflection;
+using PaintDotNet;
+using PaintDotNet.Effects;
+using PaintDotNet.IndirectUI;
+using PaintDotNet.PropertySystem;
+
+namespace AssortedPlugins.CleanTransparent
+{
+    [PluginSupportInfo(typeof(DefaultPluginInfo))]
+    public class CleanTransparent : PropertyBasedEffect
+    {
+        private int exampleProperty;
+
+        public CleanTransparent() : base(
+            typeof(CleanTransparent).Assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title,
+            new Bitmap(typeof(CleanTransparent), "icon.png"),
+            SubmenuNames.Render,
+            new EffectOptions() { Flags = EffectFlags.Configurable })
+        {
+        }
+
+        protected override ControlInfo OnCreateConfigUI(PropertyCollection props)
+        {
+            ControlInfo configUI = CreateDefaultConfigUI(props);
+
+            configUI.SetPropertyControlType(nameof(exampleProperty), PropertyControlType.ColorWheel);
+            configUI.SetPropertyControlValue(nameof(exampleProperty), ControlInfoPropertyNames.DisplayName, "Example Property");
+
+            return configUI;
+        }
+
+        protected override PropertyCollection OnCreatePropertyCollection()
+        {
+            List<Property> props = new List<Property>();
+            props.Add(new Int32Property(nameof(exampleProperty), (int)(uint)EnvironmentParameters.PrimaryColor));
+
+            return new PropertyCollection(props);
+        }
+
+        protected override void OnCustomizeConfigUIWindowProperties(PropertyCollection props)
+        {
+            base.OnCustomizeConfigUIWindowProperties(props);
+            props[ControlInfoPropertyNames.WindowTitle].Value = typeof(CleanTransparent).Assembly.GetCustomAttribute<AssemblyTitleAttribute>().Title;
+        }
+
+        protected override void OnSetRenderInfo(PropertyBasedEffectConfigToken newToken, RenderArgs dstArgs, RenderArgs srcArgs)
+        {
+            base.OnSetRenderInfo(newToken, dstArgs, srcArgs);
+            exampleProperty = newToken.GetProperty<Int32Property>(nameof(exampleProperty)).Value;
+        }
+
+        protected override void OnRender(Rectangle[] renderRects, int startIndex, int length)
+        {
+            int endIndex = startIndex + length;
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                Render(DstArgs.Surface, SrcArgs.Surface, renderRects[i]);
+            }
+        }
+
+        void Render(Surface dst, Surface src, Rectangle rect)
+        {
+        }
+    }
+}
