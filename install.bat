4e0c3ad72a9c6f634d80209994cd9dd77781329d
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-04-22 01:29:29 +0100

    Add basic installer.bat

---
+++ install.bat
@@ -0,0 +1,18 @@
+@echo off
+set /p store=Install to Windows Store version of paint.net? (Y/N)
+
+if %store% == y set store=Y
+if %store% == yes set store=Y
+
+if %store% == Y (
+  set folder="%USERPROFILE%\Documents\paint.net App Files\Effects\"
+  echo Installing to Windows Store version...
+) else (
+  set folder="%ProgramFiles%\paint.net\Effects\"
+  echo Installing to standard version...
+)
+
+if not exist %folder% mkdir %folder%
+copy *.dll %folder%
+
+pause
