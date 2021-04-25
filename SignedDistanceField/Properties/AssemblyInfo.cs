bee3d8e288759e05aaa7b110855bc6e49fd8aa3a
Sam McCreery <4602020+mccreery@users.noreply.github.com>
2021-04-25 22:14:16 +0100

    Empty project for signed distance

---
+++ SignedDistanceField/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+// See SharedAssemblyInfo.cs for other attributes
+[assembly: AssemblyTitle("SignedDistanceField")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyVersion("1.0")]
+
+[assembly: AssemblyConfiguration("tag 1|tag 2")]
